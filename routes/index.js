11c7cc26fee04395b4088db162958b524684b5c0
ExRufus <akmaldyn02@gmail.com>
2023-03-22 21:08:26 +0700

    session 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
+const pagesController = require('../controller/pageController')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', pagesController.index);
+router.get('/detail', pagesController.detail);
 
 module.exports = router;
